0aa30063b24d94c598723cdda87b088971a84abb
André Perretto de Andrade <andre.andrade@intelecta.com.br>
2017-09-11 15:22:58 -0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'lojacloud-app',
+    template: '<h1 style="font-size:170%">Loja Cloud</h1>'
+})
+export class AppComponent {}
